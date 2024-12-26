782baac222474e959374e55163c5885dcc616a76
PiotrJerzy13 <138906583+PiotrJerzy13@users.noreply.github.com>
2024-12-26 07:39:30 +0100

    changing ClapTrap Header adding Class Setters/Getters

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -22,6 +22,18 @@ public:
     void attack(const std::string& target);
     void takeDamage(unsigned int amount);
     void beRepaired(unsigned int amount);
+
+	// Accessor Methods (Getters and Setters)
+protected:
+    void setHitPoints(unsigned int hp) { hitPoints = hp; }
+    void setEnergyPoints(unsigned int ep) { energyPoints = ep; }
+    void setAttackDamage(unsigned int ad) { attackDamage = ad; }
+    void setName(const std::string& n) { name = n; }
+
+    unsigned int getHitPoints() const { return hitPoints; }
+    unsigned int getEnergyPoints() const { return energyPoints; }
+    unsigned int getAttackDamage() const { return attackDamage; }
+    std::string getName() const { return name; }
 };
 
 #endif
